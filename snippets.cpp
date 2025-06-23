40690334071963855eebbf24dc363d8d7588483c
Tamal Kanti sarker <159719424+tamalkantisarker@users.noreply.github.com>
2025-06-23 13:37:44 +0600

    Add files via upload

---
+++ snippets.cpp
@@ -0,0 +1,39 @@
+//This is the code binary exponentiation
+//#include<bits/stdc++.h>
+//using namespace std;
+//typedef long long ll;
+//const int M = 1e9+7;
+//ll binex(ll a, ll b){
+//if(b==0){
+   //return 1;
+//}
+//ll x = binex(a,(b/2));
+//if(b%2==0){
+   //return ((x*1LL*x)%M);
+//}
+//else{
+   //return ((a*((x*1LL*x)%M))%M);
+//}
+//}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(), (x).end()
+#define yes cout <<"YES\n";
+#define no cout <<"NO\n";
+const int N = 1e5+10;
+int solve(){
+          
+          
+     return 0;
+}
+int main()
+{
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+      ll t;
+      cin>>t;
+      while(t--){
+      solve();
+}
+       return 0;
+}
